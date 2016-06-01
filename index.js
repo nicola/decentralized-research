4e19418a2d7d8b8e2830d5a13301f1c76b63da8f
nicola <me@nicola.io>
2016-06-01 14:51:46 -0400

    first title was carrying a space

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ console.log('Papers I am reading\n')
 
 const folders = fs.readdirSync('./bibs').filter(f => f !== '.DS_Store')
 folders.reverse().forEach(folder => {
-  console.log('\n', '##', folder.replace(/^[0-9]+ /, ''), '\n')
+  console.log('\n##', folder.replace(/^[0-9]+ /, ''), '\n')
   const bibs = fs.readdirSync(path.join('./bibs/', folder)).filter(f => f !== '.DS_Store')
   bibs.forEach(bib => {
     let obj = parse(fs.readFileSync(path.join('./bibs/', folder, bib), 'utf8'))[0]
